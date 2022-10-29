716e3428d0a6c7910a7a5165c1c30452f65105e3
Aleksandr Prusov <iblaaaz@gmail.com>
2022-10-29 21:12:19 +0400

    scheme-001: update algos (#1)

--- key_test.go
+++ key_test.go
@@ -6,22 +6,6 @@ import (
 	"gotest.tools/assert"
 )
 
-func TestKeyHasHints(t *testing.T) {
-	assert.Check(t, (&key{"test", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"key1_test", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"test_test", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"test_key1", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"test__key1", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"key1_test_key2", ""}).hasHints([]string{"test"}))
-	assert.Check(t, (&key{"key1_test_key2", ""}).hasHints([]string{"notfound", "test"}))
-	assert.Check(t, (&key{"key1_test_key2", ""}).hasHints([]string{"test", "notfound"}))
-	assert.Check(t, (&key{"key1_test_key2", ""}).hasHints([]string{"key1", "key2"}))
-	assert.Check(t, (&key{"key1_test_key2", ""}).hasHints([]string{"key1", "test", "key2"}))
-	assert.Check(t, (&key{"KEY1_TEST_KEY2", ""}).hasHints([]string{"key1", "test", "key2"}))
-	assert.Check(t, !(&key{"key1_test_key2", ""}).hasHints([]string{""}))
-	assert.Check(t, !(&key{"key1_test_key2", ""}).hasHints([]string{"notfound"}))
-}
-
 func TestKeyString(t *testing.T) {
 	assert.Equal(t, (&key{"test", ""}).String(), "test")
 	assert.Equal(t, (&key{"key1_test_key2", ""}).String(), "key1_test_key2")
@@ -29,67 +13,42 @@ func TestKeyString(t *testing.T) {
 	assert.Equal(t, (&key{"", ""}).String(), "")
 }
 
-func checkReplaceHint(t *testing.T, h *hintedKey, matched bool, result string, matches []string) {
-	assert.Equal(t, h.matched, matched)
-	assert.Equal(t, h.name, result)
-	assert.DeepEqual(t, h.matches, matches)
-}
-
-func TestKeyReplaceHints(t *testing.T) {
-	checkReplaceHint(t,
-		(&key{"string", ""}).replaceHints([]string{"test"}),
-		false, "string", []string{},
+func TestKeyFindSimilars(t *testing.T) {
+	checkFindSimilars(t, &key{"A_B", ""},
+		[]string{"A_C", "A_D", "A_E"},
+		[]string{"A_C", "A_D", "A_E"},
 	)
-	checkReplaceHint(t,
-		(&key{"string", ""}).replaceHints([]string{"string"}),
-		true, "XXX", []string{"string"},
+	checkFindSimilars(t, &key{"A_B", ""},
+		[]string{"A_C", "A_D", "A_E", "C_D", "A", "B"},
+		[]string{"A_C", "A_D", "A_E"},
 	)
-	checkReplaceHint(t,
-		(&key{"string_key", ""}).replaceHints([]string{"key"}),
-		true, "string_XXX", []string{"key"},
+	checkFindSimilars(t, &key{"A", ""},
+		[]string{"A_B", "A_C", "C_A_B", "C_B"},
+		[]string{"A_B", "A_C"},
 	)
-	checkReplaceHint(t,
-		(&key{"string_key_key", ""}).replaceHints([]string{"key"}),
-		true, "string_XXX_XXX", []string{"key"},
+	checkFindSimilars(t, &key{"A_B_C", ""},
+		[]string{"A_B_D", "A_B_E", "A_D_E"},
+		[]string{"A_B_D", "A_B_E"},
 	)
-	checkReplaceHint(t,
-		(&key{"KEY1_TEST_KEY2", ""}).replaceHints([]string{"key1", "key2"}),
-		true, "XXX_test_XXX", []string{"key1", "key2"},
+	checkFindSimilars(t, &key{"A", ""},
+		[]string{"B", "C"},
+		nil,
 	)
-	checkReplaceHint(t,
-		(&key{"KEY_KEY1_KEY2", ""}).replaceHints([]string{"key"}),
-		true, "XXX_key1_key2", []string{"key"},
+	checkFindSimilars(t, &key{"A_B_C", ""},
+		[]string{"D_E_F", "G_H_I"},
+		nil,
 	)
 }
 
-func checkFindSimilars(t *testing.T, k *key, c []string, hints []string, answer []string) {
+func checkFindSimilars(t *testing.T, k *key, c []string, answer []string) {
 	candidates := []*key{}
 	for _, candidate := range c {
 		candidates = append(candidates, &key{candidate, ""})
 	}
 	var result []string
-	resultKeys := k.findSimilars(candidates, hints)
+	resultKeys := k.findSimilars(candidates)
 	for _, r := range resultKeys {
 		result = append(result, r.name)
 	}
 	assert.DeepEqual(t, answer, result)
-}
-func TestKeyFindSimilars(t *testing.T) {
-	checkFindSimilars(t, &key{"DATABASE_PORT", ""},
-		[]string{"DATABASE_PORT", "DATABASE_HOST", "DATABASE_USER"}, []string{"user", "port", "host"},
-		[]string{"DATABASE_PORT", "DATABASE_HOST", "DATABASE_USER"},
-	)
-	checkFindSimilars(t, &key{"DATABASE", ""},
-		[]string{"DATABASE_PORT", "DATABASE_HOST", "DATABASE_USER"}, []string{"user", "port", "host"},
-		nil,
-	)
-	checkFindSimilars(t, &key{"HOST_PORT", ""},
-		[]string{"HOST_USER", "HOST_HOST", "HOST_PASS", "CLIENT_ID"}, []string{"host", "user", "port", "pass"},
-		[]string{"HOST_USER", "HOST_HOST", "HOST_PASS"},
-	)
-	checkFindSimilars(t, &key{"API_ENDPOINT", ""},
-		[]string{"API_USER", "API_PASS"}, []string{"endpoint", "user", "pass"},
-		[]string{"API_USER", "API_PASS"},
-	)
-
 }
