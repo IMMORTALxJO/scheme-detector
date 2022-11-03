886d9312be783d3f71f89420beb4fab5848c4d1d
Aleksandr Prusov <iblaaaz@gmail.com>
2022-11-03 16:46:35 +0300

    scheme-002: update detection logic

--- key_test.go
+++ key_test.go
@@ -7,34 +7,42 @@ import (
 )
 
 func TestKeyString(t *testing.T) {
-	assert.Equal(t, (&key{"test", ""}).String(), "test")
-	assert.Equal(t, (&key{"key1_test_key2", ""}).String(), "key1_test_key2")
-	assert.Equal(t, (&key{"KEY1_TEST_KEY2", ""}).String(), "KEY1_TEST_KEY2")
-	assert.Equal(t, (&key{"", ""}).String(), "")
+	assert.Equal(t, (newKey("test", "")).String(), "test")
+	assert.Equal(t, (newKey("key1_test_key2", "")).String(), "key1_test_key2")
+	assert.Equal(t, (newKey("KEY1_TEST_KEY2", "")).String(), "KEY1_TEST_KEY2")
+	assert.Equal(t, (newKey("", "")).String(), "")
 }
 
 func TestKeyFindSimilars(t *testing.T) {
-	checkFindSimilars(t, &key{"A_B", ""},
+	checkFindSimilars(t, newKey("A_B", ""),
+		[]string{"A_B"},
+		[]string{"A_B"},
+	)
+	checkFindSimilars(t, newKey("A_B", ""),
 		[]string{"A_C", "A_D", "A_E"},
 		[]string{"A_C", "A_D", "A_E"},
 	)
-	checkFindSimilars(t, &key{"A_B", ""},
+	checkFindSimilars(t, newKey("A_B", ""),
 		[]string{"A_C", "A_D", "A_E", "C_D", "A", "B"},
-		[]string{"A_C", "A_D", "A_E"},
+		[]string{"A_C", "A_D", "A_E", "A"},
 	)
-	checkFindSimilars(t, &key{"A", ""},
+	checkFindSimilars(t, newKey("A_B", ""),
+		[]string{"A_C", "A_D", "A_E", "A_B_C", "C_D", "A", "B"},
+		[]string{"A_C", "A_D", "A_E", "A_B_C", "A"},
+	)
+	checkFindSimilars(t, newKey("A", ""),
 		[]string{"A_B", "A_C", "C_A_B", "C_B"},
-		[]string{"A_B", "A_C"},
+		[]string{"A_B", "A_C", "C_A_B"},
 	)
-	checkFindSimilars(t, &key{"A_B_C", ""},
+	checkFindSimilars(t, newKey("A_B_C", ""),
 		[]string{"A_B_D", "A_B_E", "A_D_E"},
-		[]string{"A_B_D", "A_B_E"},
+		[]string{"A_B_D", "A_B_E", "A_D_E"},
 	)
-	checkFindSimilars(t, &key{"A", ""},
+	checkFindSimilars(t, newKey("A", ""),
 		[]string{"B", "C"},
 		nil,
 	)
-	checkFindSimilars(t, &key{"A_B_C", ""},
+	checkFindSimilars(t, newKey("A_B_C", ""),
 		[]string{"D_E_F", "G_H_I"},
 		nil,
 	)
@@ -43,7 +51,7 @@ func TestKeyFindSimilars(t *testing.T) {
 func checkFindSimilars(t *testing.T, k *key, c []string, answer []string) {
 	candidates := []*key{}
 	for _, candidate := range c {
-		candidates = append(candidates, &key{candidate, ""})
+		candidates = append(candidates, newKey(candidate, ""))
 	}
 	var result []string
 	resultKeys := k.findSimilars(candidates)
